aaf4cd4e6a7527befc7cfaf768d717e5e18f3ed4
puchol <puchol@me.com>
2022-04-13 23:25:28 +0200

    Fix for util.find_library in Python 3.10

--- iperf3/iperf3.py
+++ iperf3/iperf3.py
@@ -97,7 +97,7 @@ class IPerf3(object):
         :param lib_name: optional name and path for libiperf.so.0 library
         """
         if lib_name is None:
-            lib_name = util.find_library('libiperf')
+            lib_name = util.find_library('iperf')
             if lib_name is None:
                 # If we still couldn't find it lets try the manual approach
                 lib_name = 'libiperf.so.0'
